19e24fecf35106f9fcf1011abd36935ac4bf4416
Myung Choi <myungchoi@gmail.com>
2022-09-29 10:13:34 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'myst_parser'
 ]
 
 intersphinx_mapping = {
@@ -25,9 +26,18 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+def setup(app):
+    '''Custom CSS for Width of RTD'''
+    app.add_css_file('my_theme.css')
